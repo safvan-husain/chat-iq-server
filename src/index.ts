cb9840e66834ac3262eed9e14028c8d57039db9f
safvan-husain <safvanhusain009@gmail.com>
2023-10-28 15:38:24 +0530

    portfolio added

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import bodyParser from "body-parser";
 import { SignUpRouter } from "./routes/sign_up_route";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+
 import { DataRouter } from "./routes/get_data";
 import { onWebSocket } from "./utils/web_socket";
 import { ProfileRouter } from "./routes/user_account";
@@ -14,10 +18,13 @@ import { messageRouter } from "./routes/message_router";
 dotenv.config();
 const app = express();
 
+app.use(logger("dev"));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public-flutter")));
 app.use(SigninRouter);
 app.use(SignUpRouter);
 app.use(messageRouter);
@@ -25,7 +32,7 @@ app.use(DataRouter);
 app.use(ProfileRouter);
 
 app.get("/", (req, res) => {
-  res.send("privacy policy");
+  res.sendFile(path.join(__dirname, "public-flutter", "index.html"));
 });
 
 mongoose.set("strictQuery", true);
